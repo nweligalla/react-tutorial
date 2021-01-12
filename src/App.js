233481fc0dae720e25b664fcc6c17414de70572d
nweligalla <nayanaweligalla@gmail.com>
2021-01-12 13:38:57 +0530

    tuorial about class components and functional components with props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet heroName="Batman" name="Bruce">
+        <p>This is a children property</p>
+      </Greet>
+      <Greet heroName="Super" name="Clark">
+        <button>click me</button>
+      </Greet>
+      <Greet heroName="Wonder Woman" name="Diana" />
+      <Welcome heroName="Batman" name="Bruce" />
+      <Welcome heroName="Superman" name="Clark" />
+      <Welcome heroName="Wonder Woman" name="Diana" />
     </div>
   );
 }

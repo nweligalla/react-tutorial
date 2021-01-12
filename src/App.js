f4b047f4fae2d046699dde5aa3790673a504013b
nweligalla <nayanaweligalla@gmail.com>
2021-01-12 16:30:14 +0530

    Event bind approaches

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Message from "./components/Message";
 import Counter from "./components/Counter";
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
 
 function App() {
   return (
     <div className="App">
-      <ClassClick/>
+      <EventBind />
+      {/* <ClassClick/> */}
       {/* <FunctionClick /> */}
       {/* <Counter /> */}
       {/* <Message /> */}
